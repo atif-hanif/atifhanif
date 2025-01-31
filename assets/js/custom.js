ff4435bff6a475e8ad291a0ca3147a095e98d0b2
atif-hanif <atifhanif74@gmail.com>
2025-01-31 14:40:32 +0500

    31-02-2025 (2)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,67 +17,14 @@ $(document).ready(function () {
         strings: ["Designer", "Developer"],
         breakLines: false,
     }).go();
-});
 
-(function($) {
-    "use strict";
-    var WEA = {};
-    var plugin_track = 'assets/vendors/';
-    $.fn.exists = function() {
-        return this.length > 0;
-    };
-
-    WEA.one_page = function() {
-        //var HHeight = $('.navbar').outerHeight();
-        var $one_page_nav = $('.one-page-nav');
-        if ($one_page_nav.length > 0) {
-            $one_page_nav.each(function() {
-                $.scrollIt({
-                    upKey: 38, // key code to navigate to the next section
-                    downKey: 40, // key code to navigate to the previous section
-                    easing: 'linear', // the easing function for animation
-                    scrollTime: 600, // how long (in ms) the animation takes
-                    activeClass: 'active', // class given to the active nav element
-                    onPageChange: null, // function(pageIndex) that is called when page is changed
-                    topOffset: -40 // offste (in px) for fixed top navigation
-                });
-            });
-        }
-    }
-
-
-    /* ---------------------------------------------- /*
-     * All Functions
-    /* ---------------------------------------------- */
-    // loadScript
-    var _arr = {};
-
-    function loadScript(scriptName, callback) {
-        if (!_arr[scriptName]) {
-            _arr[scriptName] = true;
-            var body = document.getElementsByTagName('body')[0];
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = scriptName;
-            // WEAn bind WEA event to WEA callback function
-            // WEAre are several events for cross browser compatibility
-            // script.onreadystatechange = callback;
-            script.onload = callback;
-            // fire WEA loading
-            body.appendChild(script);
-        } else if (callback) {
-            callback();
-        }
-    };
-
-    // Document on Ready
-    $(document).ready(function() {
-            WEA.one_page();
+    $.scrollIt({
+        upKey: 38,
+        downKey: 40,
+        easing: 'linear',
+        scrollTime: 600,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset: -40
     });
-
-
-    // Window on Resize
-    $(window).on("resize", function() {});
-
-
-})(jQuery);
+});
